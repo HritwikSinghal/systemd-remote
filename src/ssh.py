856be8b8a4e9f182145b8ba5c5d9c71679032c73
Hritwik <hritwik.s@media.net>
2022-09-14 14:06:32 +0530

    get ssh key and password via .env

--- src/ssh.py
+++ src/ssh.py
@@ -2,15 +2,17 @@ import os
 
 import paramiko
 
-
 # todo: get host name:port list from file
 # todo: get ssh key, pass from file
 # todo: multi-threaded
 
+SSH_KEY_FILE = os.getenv("SSH_KEY_FILE")
+SSH_KEY_FILE_PASSWORD = os.getenv("SSH_KEY_FILE_PASSWORD")
+
 
 def get_process_list() -> list:
     ssh = paramiko.SSHClient()
-    k = paramiko.RSAKey.from_private_key_file("/home/hritwik/.ssh/id_rsa", password=os.getenv("PASSWORD"))
+    k = paramiko.RSAKey.from_private_key_file(filename=SSH_KEY_FILE, password=SSH_KEY_FILE_PASSWORD)
     ssh.set_missing_host_key_policy(paramiko.AutoAddPolicy())
     ssh.connect('172.105.47.207', username="root", pkey=k)
     ssh_stdin, ssh_stdout, ssh_stderr = ssh.exec_command("systemctl list-units --no-page")
