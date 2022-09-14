398db57929fe5223ff7d28e26301f3328ca4d00e
Hritwik <hritwik.s@media.net>
2022-09-14 13:47:20 +0530

    refactor

--- main.py
+++ main.py
@@ -1,22 +1,28 @@
 from dotenv import load_dotenv
 
-from src.ssh import get_ssh_data
+from src.ssh import get_process_list
 
+# load env variables
 load_dotenv()
 
 
 def start() -> None:
-    out: list = get_ssh_data()
-    dic = {}
-    for i in out:
-        print(i)
-        dic[i[0]] = {
+    process_list: list = get_process_list()
+    # for process in process_list:
+    #     print(process)
+    # return
+    formatted_output_dict = {}
+    for i in process_list:
+        formatted_output_dict[i[0]] = {
             "load": i[1],
             "active": i[2],
             "sub": i[3],
             "desc": " ".join(i[4:])
         }
-    print(dic)
+    print("\n\n\n")
+    for k in formatted_output_dict:
+        print(k, formatted_output_dict[k])
+    # print(formatted_output_dict)
 
 
 if __name__ == '__main__':
--- src/ssh.py
+++ src/ssh.py
@@ -3,9 +3,14 @@ import os
 import paramiko
 
 
-def get_ssh_data() -> list:
+# todo: get host name:port list from file
+# todo: get ssh key, pass from file
+# todo: multi-threaded
+
+
+def get_process_list() -> list:
     ssh = paramiko.SSHClient()
-    k = paramiko.RSAKey.from_private_key_file("/home/xpert/.ssh/id_rsa", password=os.getenv("PASSWORD"))
+    k = paramiko.RSAKey.from_private_key_file("/home/hritwik/.ssh/id_rsa", password=os.getenv("PASSWORD"))
     ssh.set_missing_host_key_policy(paramiko.AutoAddPolicy())
     ssh.connect('172.105.47.207', username="root", pkey=k)
     ssh_stdin, ssh_stdout, ssh_stderr = ssh.exec_command("systemctl list-units --no-page")
