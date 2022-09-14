f1820afb816c1e7abd0240d7e23de3e1592730e1
Hritwik <hritwik.s@media.net>
2022-09-14 13:30:30 +0530

    refactor

---
+++ src/ssh.py
@@ -0,0 +1,15 @@
+import os
+
+import paramiko
+
+
+def get_ssh_data() -> list:
+    global out
+    ssh = paramiko.SSHClient()
+    k = paramiko.RSAKey.from_private_key_file("/home/xpert/.ssh/id_rsa", password=os.getenv("PASSWORD"))
+    ssh.set_missing_host_key_policy(paramiko.AutoAddPolicy())
+    ssh.connect('172.105.47.207', username="root", pkey=k)
+    ssh_stdin, ssh_stdout, ssh_stderr = ssh.exec_command("systemctl list-units --no-page")
+    out: list = ssh_stdout.readlines()[1:-7]
+    out = list(map(lambda x: x.split(), out))
+    return out
