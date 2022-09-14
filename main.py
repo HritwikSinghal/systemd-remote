a86a12e3e822d20de107a03c5427a64b26772f45
Hritwik <hritwik.s@media.net>
2022-09-14 13:31:27 +0530

    add name main block

--- main.py
+++ main.py
@@ -1,28 +1,23 @@
-import paramiko
-import os
 from dotenv import load_dotenv
+
+from src.ssh import get_ssh_data
 
 load_dotenv()
 
-ssh = paramiko.SSHClient()
-k = paramiko.RSAKey.from_private_key_file("/home/xpert/.ssh/id_rsa", password=os.getenv("PASSWORD"))
 
-ssh.set_missing_host_key_policy(paramiko.AutoAddPolicy())
-ssh.connect('172.105.47.207', username="root", pkey=k)
+def start() -> None:
+    out: list = get_ssh_data()
+    dic = {}
+    for i in out:
+        print(i)
+        dic[i[0]] = {
+            "load": i[1],
+            "active": i[2],
+            "sub": i[3],
+            "desc": " ".join(i[4:])
+        }
+    print(dic)
 
-ssh_stdin, ssh_stdout, ssh_stderr = ssh.exec_command("systemctl list-units --no-page")
-out: list = ssh_stdout.readlines()[1:-7]
-out = list(map(lambda x: x.split(), out))
 
-dic = {}
-
-for i in out:
-    print(i)
-    dic[i[0]] = {
-        "load": i[1],
-        "active": i[2],
-        "sub": i[3],
-        "desc": " ".join(i[4:])
-    }
-
-print(dic)
+if __name__ == '__main__':
+    start()
